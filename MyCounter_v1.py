ff228e7d763ea61efcb875fbc2b231a4ae688d73
TannaiTomoya <tomobou5912@gmail.com>
2026-01-14 18:41:39 +0900

    fix: MyCounterV1 修正）

--- MyCounter_v1.py
+++ MyCounter_v1.py
@@ -1,24 +1,26 @@
-import math
+class MyCounterV1:
+    def __init__(self, value):
+        self.value = value
+
+    def count_up(self):
+        self.value += 1
 
 
-class Rectangle:
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
+# 動作確認（課題文どおり）
+counter1 = MyCounterV1(value=0)
+print(counter1.value)  # 0
 
-    # 30.00 / 9.00 を出すため "文字列" で返す
-    def area(self):
-        return f"{self.height * self.width:.2f}"
+counter1.count_up()
+print(counter1.value)  # 1
 
-    def diagonal(self):
-        return round(math.sqrt(self.height**2 + self.width**2), 2)
+counter1.count_up()
+print(counter1.value)  # 2
 
+counter2 = MyCounterV1(value=7)
+print(counter2.value)  # 7
 
-if __name__ == "__main__":
-    rectangle1 = Rectangle(height=5, width=6)
-    print(rectangle1.area())  # 30.00
-    print(rectangle1.diagonal())  # 7.81
+counter2.count_up()
+print(counter2.value)  # 8
 
-    rectangle2 = Rectangle(height=3, width=3)
-    print(rectangle2.area())  # 9.00
-    print(rectangle2.diagonal())  # 4.24
+counter2.count_up()
+print(counter2.value)  # 9
